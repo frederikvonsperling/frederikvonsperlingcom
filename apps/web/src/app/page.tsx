ce7ca90fdf6fe7397f48e9fa7092ab717d43a911
frederiknygaardadapt <frederik.nygaard@adaptagency.com>
2023-08-20 19:53:08 +0200

    hey

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,21 +1,9 @@
-import { createClient } from "@sanity/client";
+import { css } from "styled-system/css";
 
 export default async function Page() {
-  const client2 = createClient({
-    projectId: "0ihtgybt",
-    dataset: "production",
-    useCdn: true,
-    apiVersion: "v2021-10-21",
-    token:
-      "skGKcEmNbd24JY7Ff0MekzaR1ZhcwRDqXCmdWAVMwrMDc7fGIIUjRpCmgpuXH67ogxuv7jEPhQtJKtahQc39TmzbyR5rDSGDClT2tJ5uWYp7QrwOeHZNuxR6PWa8EfkLEhlkAnGR4Ags0gMUYYacagiz2OrCyyBtUNvqvW7GFGQ9CvUx66ed",
-  });
-
-  const posts = await client2.fetch("*[_type == 'post']");
-
   return (
     <>
-      {/* <Header text="Web" /> */}
-      {/* <Button /> */}
+      <p className={css({ color: "white" })}>Hey!</p>
     </>
   );
 }
